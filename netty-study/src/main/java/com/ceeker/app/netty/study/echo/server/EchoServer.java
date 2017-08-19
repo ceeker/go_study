0968d7281de3ae010f2319514ce7bc1a2fc89341
vectorzhang <409273291@qq.com>
2017-08-19 09:48:25 +0800

    调整注释

--- netty-study/src/main/java/com/ceeker/app/netty/study/echo/server/EchoServer.java
+++ netty-study/src/main/java/com/ceeker/app/netty/study/echo/server/EchoServer.java
@@ -3,6 +3,7 @@ package com.ceeker.app.netty.study.echo.server;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -36,7 +37,7 @@ public class EchoServer {
     }
 
     public void start() throws Exception {
-        NioEventLoopGroup group = new NioEventLoopGroup(); //3
+        EventLoopGroup group = new NioEventLoopGroup(); //3
         try {
             ServerBootstrap b = new ServerBootstrap();
             b.group(group)                                //4
